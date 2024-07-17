25e518b0386c4cafa6dd777389e7e7137faf39e4
sridharshiniArumugam <sridharshiniece@gmail.com>
2024-07-17 22:43:39 +0530

    commit

--- Day3.js
+++ Day3.js
@@ -37,7 +37,13 @@ else{
  }
  
  //switch case 
- let day =1;
- switch(day){
- case 1: 
+
+ switch(new Date().getDay()){
+ case 0: console.log("Sunday"); break;
+ case 1: console.log("Monday"); break;
+ case 2: console.log("Tuesday"); break;
+ case 3: console.log("Wednesday"); break;
+ case 4: console.log("Thursday"); break;
+ case 5: console.log("Friday"); break;
+ case 6: console.log("saturday");
  }
